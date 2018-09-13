d04b7fc87cc5dd4e5ec737ab7bae1dbe34fd87ce
tentenponce <poncetenten10@gmail.com>
2018-09-13 16:38:13 +0800

    Updated independent number dialog

--- ChatBotWorkshop.3.Dialog/app.js
+++ ChatBotWorkshop.3.Dialog/app.js
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
+A simple echo bot for the Microsoft Bot Framework.
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
@@ -9,9 +9,9 @@ var botbuilder_azure = require("botbuilder-azure");
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -19,11 +19,11 @@ var connector = new builder.ChatConnector({
     openIdMetadata: process.env.BotOpenIdMetadata
 });
 
-// Listen for messages from users 
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* Bot Storage: This is a great spot to register the private state storage for your bot.
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
@@ -37,64 +37,40 @@ var bot = new builder.UniversalBot(connector);
 // bot.set('storage', tableStorage);
 
 bot.dialog('/', [function(session) {
-    builder.Prompts.choice(session, 'Please select a Math operation', ['Add','Subtract']);
-}, function(session, results){
-
-    var mathOperation = results.response.entity;
-    switch(mathOperation) {
-        case 'Add':
-            session.beginDialog('addDialog');
-            break;
-        case 'Subtract':
-            session.beginDialog('subtractDialog');
-            break;
-    }
-}]);
-
-bot.dialog('addDialog', [function(session) {
-    builder.Prompts.number(session, 'Enter first number');
+  builder.Prompts.choice(session, 'Please select a Math operation', ['Add','Subtract', 'Multiply', 'Divide']);
+}, function(session, results) {
+  session.dialogData.operator = results.response.entity;
+  session.beginDialog('numberDialog');
 }, function (session, results) {
+  const answer = doTheMath(results.firstNumber, results.secondNumber, session.dialogData.operator);
 
-    session.dialogData.firstNumber = results.response;
-    builder.Prompts.number(session, 'Enter second number');
+  session.send(`The answer is: ${answer}`);
+  session.endDialog();
+}])
+
+bot.dialog('numberDialog', [function(session) {
+  builder.Prompts.number(session, 'Enter first number');
 }, function (session, results) {
+  session.dialogData.firstNumber = results.response;
 
-    session.dialogData.secondNumber = results.response;
-    var sum = doTheMath(session.dialogData.firstNumber, 
-                        session.dialogData.secondNumber,
-                        'Add');
-
-    session.send('The sum is: ' + sum);
-    session.endDialog();
-}]).triggerAction({
-    matches: /^add$/i
-});
-
-bot.dialog('subtractDialog', [function(session) {
-    builder.Prompts.number(session, 'Enter first number');
+  builder.Prompts.number(session, 'Enter second number');
 }, function (session, results) {
+  session.dialogData.secondNumber = results.response;
 
-    session.dialogData.firstNumber = results.response;
-    builder.Prompts.number(session, 'Enter second number');
-}, function (session, results) {
-
-    session.dialogData.secondNumber = results.response;
-    var difference = doTheMath(session.dialogData.firstNumber, 
-                        session.dialogData.secondNumber,
-                        'Subtract');
-
-    session.send('The difference is: ' + difference);
-    session.endDialog();
-}]);
+  session.endDialogWithResult(session.dialogData);
+}])
 
 function doTheMath(firstNumber, secondNumber, mathOperation) {
-    switch(mathOperation) {
-        case 'Add':
-            return firstNumber + secondNumber;
-        case 'Subtract':
-            return firstNumber - secondNumber;
-        default:
-            return null;
-    }
+  switch(mathOperation) {
+    case 'Add':
+      return firstNumber + secondNumber;
+    case 'Subtract':
+      return firstNumber - secondNumber;
+    case 'Multiply':
+      return firstNumber * secondNumber;
+    case 'Divide':
+      return firstNumber / secondNumber;
+    default:
+      return null;
+  }
 }
-
